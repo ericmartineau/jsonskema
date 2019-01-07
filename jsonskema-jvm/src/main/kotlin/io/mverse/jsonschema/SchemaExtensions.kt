52becdf4e2bbb1121ccaaf0938cf71340493a8ed
ericmartineau <smartytime@gmail.com>
2019-01-06 21:23:24 -0700

    Adding stack overflow protection for recursive function

--- jsonskema-jvm/src/main/kotlin/io/mverse/jsonschema/SchemaExtensions.kt
+++ jsonskema-jvm/src/main/kotlin/io/mverse/jsonschema/SchemaExtensions.kt
@@ -9,7 +9,7 @@ import io.mverse.jsonschema.utils.calculateType
 val Draft7Schema.allProperties: Map<String, SchemaWithRequiredStatus>
   get() {
     val holder = mutableMapOf<String, SchemaWithRequiredStatus>()
-    return recurseProperties(this, "", true, holder)
+    return recurseProperties(this, "", true, holder, mutableSetOf())
   }
 
 data class SchemaWithRequiredStatus(val schema: Draft7Schema, val isRequired: Boolean)
@@ -17,44 +17,49 @@ data class SchemaWithRequiredStatus(val schema: Draft7Schema, val isRequired: Bo
 infix fun Draft7Schema.with(isRequired: Boolean): SchemaWithRequiredStatus = SchemaWithRequiredStatus(this, isRequired)
 
 fun recurseProperties(schema: Draft7Schema, currentPath: String, isRequired: Boolean,
-                      holder: MutableMap<String, SchemaWithRequiredStatus>): Map<String, SchemaWithRequiredStatus> {
+                      holder: MutableMap<String, SchemaWithRequiredStatus>,
+                      processed: MutableSet<String>): Map<String, SchemaWithRequiredStatus> {
 
-  // If this property is a deterministic primitive, add it
-  val calculateType = schema.calculateType()
-  if (calculateType != null && calculateType != JsonSchemaType.OBJECT && calculateType != JsonSchemaType.ARRAY &&
-      calculateType != JsonSchemaType.NULL) {
-    holder[currentPath] = schema with isRequired
-  } else {
+  if (!processed.contains(currentPath)) {
+    processed += currentPath
 
-    // Iterate properties
-    schema.properties.forEach { (path, prop) ->
-      val propIsRequired = isRequired && schema.requiredProperties.contains(path)
-      val propPath = "$currentPath/$path"
-      recurseProperties(prop.asDraft7(), propPath, propIsRequired, holder)
-    }
+    // If this property is a deterministic primitive, add it
+    val calculateType = schema.calculateType()
+    if (calculateType != null && calculateType != JsonSchemaType.OBJECT && calculateType != JsonSchemaType.ARRAY &&
+        calculateType != JsonSchemaType.NULL) {
+      holder[currentPath] = schema with isRequired
+    } else {
 
-    schema.allOfSchemas.forEach { allOf ->
-      recurseProperties(allOf.asDraft7(), currentPath, isRequired, holder)
-    }
+      // Iterate properties
+      schema.properties.forEach { (path, prop) ->
+        val propIsRequired = isRequired && schema.requiredProperties.contains(path)
+        val propPath = "$currentPath/$path"
+        recurseProperties(prop.asDraft7(), propPath, propIsRequired, holder, processed)
+      }
 
-    schema.oneOfSchemas.forEach { oneOf ->
-      recurseProperties(oneOf.asDraft7(), currentPath, false, holder)
-    }
+      schema.allOfSchemas.forEach { allOf ->
+        recurseProperties(allOf.asDraft7(), currentPath, isRequired, holder, processed)
+      }
 
-    schema.anyOfSchemas.forEach { anyOf ->
-      recurseProperties(anyOf.asDraft7(), currentPath, false, holder)
-    }
+      schema.oneOfSchemas.forEach { oneOf ->
+        recurseProperties(oneOf.asDraft7(), currentPath, false, holder, processed)
+      }
 
-    if (schema.thenSchema != null) {
-      recurseProperties(schema.thenSchema!!.asDraft7(), currentPath, false, holder)
-    }
+      schema.anyOfSchemas.forEach { anyOf ->
+        recurseProperties(anyOf.asDraft7(), currentPath, false, holder, processed)
+      }
 
-    if (schema.elseSchema != null) {
-      recurseProperties(schema.elseSchema!!.asDraft7(), currentPath, false, holder)
-    }
+      if (schema.thenSchema != null) {
+        recurseProperties(schema.thenSchema!!.asDraft7(), currentPath, false, holder, processed)
+      }
 
-    schema.allItemSchema?.run {
-      recurseProperties(this.asDraft7(), currentPath, isRequired && this.minItems ?: 0 > 0, holder)
+      if (schema.elseSchema != null) {
+        recurseProperties(schema.elseSchema!!.asDraft7(), currentPath, false, holder, processed)
+      }
+
+      schema.allItemSchema?.run {
+        recurseProperties(this.asDraft7(), currentPath, isRequired && this.minItems ?: 0 > 0, holder, processed)
+      }
     }
   }
 
