155c78956d27e0cc27f4902e517adb529c5caf11
ericmartineau <smartytime@gmail.com>
2019-06-25 14:30:30 -0700

    Cleanup tests

--- jsonskema-jvm/src/test/kotlin/io/mverse/jsonschema/assertj/asserts/Draft7Assert.kt
+++ jsonskema-jvm/src/test/kotlin/io/mverse/jsonschema/assertj/asserts/Draft7Assert.kt
@@ -1,58 +1,42 @@
 package io.mverse.jsonschema.assertj.asserts
 
-import assertk.Assert
-import assertk.all
 import assertk.assertions.isFalse
-import assertk.assertions.isNotNull
 import assertk.assertions.isTrue
-import io.mverse.jsonschema.Draft7Schema
 import io.mverse.jsonschema.keyword.Keywords
 
 fun SchemaAssert.isReadOnly(): SchemaAssert {
-  assert(actual.draft7().isReadOnly, "readOnly").all {
-    isNotNull()
-    isTrue()
-  }
+  isDraft7().transform("readOnly") { it.isReadOnly }.isTrue()
   return this
 }
 
 fun SchemaAssert.isNotReadOnly(): SchemaAssert {
-  assert(actual.draft7().isReadOnly, "readOnly").all {
-    isNotNull()
-    isFalse()
-  }
+  isDraft7().transform("readOnly") { it.isReadOnly }.isFalse()
   return this
 }
 
 fun SchemaAssert.isWriteOnly(): SchemaAssert {
-  assert(actual.draft7().isWriteOnly, "writeOnly").all {
-    isNotNull()
-    isTrue()
-  }
+  isDraft7().transform("writeOnly") { it.isWriteOnly }.isTrue()
   return this
 }
 
 fun SchemaAssert.isNotWriteOnly(): SchemaAssert {
-  assert(actual.draft7().isWriteOnly, "writeOnly").all {
-    isNotNull()
-    isTrue()
-  }
+  isDraft7().transform("writeOnly") { it.isWriteOnly }.isFalse()
   return this
 }
 
 fun SchemaAssert.hasIfSchema(): SchemaAssert {
   hasKeyword(keyword = Keywords.IF)
-  return assert(actual.draft7().ifSchema!!)
+  return transform { it.draft7().ifSchema!! }
 }
 
 fun SchemaAssert.hasThenSchema(): SchemaAssert {
   hasKeyword(keyword = Keywords.THEN)
-  return assert(actual.draft7().thenSchema!!)
+  return transform { it.draft7().thenSchema!! }
 }
 
 fun SchemaAssert.hasElseSchema(): SchemaAssert {
   hasKeyword(keyword = Keywords.ELSE)
-  return assert(actual.draft7().elseSchema!!)
+  return transform { it.draft7().elseSchema!! }
 }
 
 
